eed1b73c2e0e9ee2a85d8e3c615d868e65ff5950
KNarita <intecs.2603ap.08@gmail.com>
2026-06-04 10:06:24 +0900

    一部関数の名前の変更・デバッグメニューの制御

--- Clicker.js
+++ Clicker.js
@@ -1,7 +1,7 @@
-console.log('HelloWorld');
 const Item_text = {//表示物兼Tier数定義
     //name->ボタンやステータスバーに表示されるテキスト・text->ホバー時に表示されるテキスト
     //ここに要素を追加することでT20まで増やせます（必ずT〇の名前で順番に増やすこと）
+    //注意：T16 ~ T20はステータスが設定されていますが、数字が大きすぎるため不具合が生じる可能性があります
     //それ以上を追加する場合は上のItem_configのRankとPowerに要素を追加してください
     //名称:{name:``, text:``},
     Click: { name: `コーディング学習`, text: `効率の良いコーディング法を学びクリックによる生産量を増やす\n『プログラミング究極ガイド(税別30,000円)』` },
@@ -14,7 +14,7 @@ const Item_text = {//表示物兼Tier数定義
     T7: { name: `量子コンピューター開発`, text: `量子の特性を活かし従来の数億倍の速さで演算をします\n『効率よくバグ取りができる、効率よくバグも発生する』` },
     T8: { name: `惑星探査`, text: `地球外を探査し他の惑星にデータセンターを設立します\n『開発において真に驚くべき方法を見つけたが、\n　それをするにはこの星は狭すぎる』` },
     T9: { name: `ダイソンスフィア`, text: `恒星を包む発電機を作り莫大な電力を演算に使用します\n『太陽の光が届かなくなったが、おかげで冷却効率は上がった』` },
-    T10: { name: `人工脳`, text: `無数の人工脳細胞を培養し、夢の中でコードを書かせます\n『人工脳がコードの夢を見るのか、コードが人工脳の夢を見るのか\n　**就寝時間は労働時間に見なされません**』` },
+    T10: { name: `人工脳`, text: `無数の人工脳細胞を培養し、夢の中でコードを書かせます\n『人工脳がコードの夢を見るのか、コードが人工脳の夢を見るのか\n　*就寝時間は労働時間に見なされません*』` },
     T11: { name: `タイムマシン`, text: `過去に戻り、見つかる前のバグを修正します\n『残業代はありません、タイムカードに記録がないですからね』` },
     T12: { name: `数学的特異点`, text: `ゼロ除算を始めとする新たな理論を構築し、数学的限界を超越します\n『コードの効率化のため、まず1=2とします』` },
     T13: { name: `多元宇宙`, text: `平行世界へ接続し宇宙全体をコンピュータにします\n『---マージコンフリクトを検出---\n　クソッ、他の自分がコードを書き替えやがった！』` },
@@ -32,12 +32,15 @@ for (let i = 1; i <= (Itemkeys.length - 1); i++) {
     gameState[`T${i}Rank`] = parseInt(localStorage.getItem(`T${i}Rank`)) || 0;
     gameState[`T${i}Pow`] = parseInt(localStorage.getItem(`T${i}Pow`)) || 0;
 }
+//その他データ準備
 let isDev = parseInt(localStorage.getItem("debugFlg")) || 0;
 let lastTime = performance.now();
 let Aps = parseFloat(0);
 let nextTier = parseInt(localStorage.getItem(`nextTier`)) || 1;
 
 const Item_config = {//強化項目の数値
+    //T20以上を追加する場合はRankとPowの両方を追加してください
+    //注意：数字が大きすぎるためT16以降の挙動は不具合が発生する場合があります
     //BaceCost->基本価格・multipul->価格の上昇倍率・Pow->1購入あたりの秒間生産量
     ClRank: { BaceCost: 20, multipul: 3.4, },
     //前のTierの13倍の価格・15倍のパワー
@@ -95,7 +98,7 @@ const game = new Proxy(gameState, {
             while (Item_config[`T${nextTier}Rank`]) {//所持金に応じたTierの解放
                 const targetConfig = Item_config[`T${nextTier}Rank`];
                 // そのTierの基本価格の30%のお金が溜まったら強化の表示解放
-                if (value >= targetConfig.BaceCost * 0.3) {
+                if (value >= (targetConfig.BaceCost * 0.3)) {
                     unlockTierDOM(nextTier);
                     nextTier++;
                 } else {
@@ -105,30 +108,30 @@ const game = new Proxy(gameState, {
         }
         if (prop === 'ClRank') {
             document.querySelector('.ClPow').textContent = `${Item_text['Click']['name']}: ${Math.floor(value + 1)}`;
-            let Cost = CalCost('ClRank', value);
+            let Cost = calCost('ClRank', value);
             document.querySelector('.UpgradeClk').value = `${Item_text['Click']['name']} (＄: ${formatNum(Cost)})`;
         }
         if (/^T\d+Rank$/.test(prop)) {
             const tier = prop.replace('Rank', "");
             document.querySelector(`.${tier}`).textContent = `${Item_text[tier]['name']}: ${Math.floor(value)}`;
-            let Cost = CalCost(prop, value);
+            let Cost = calCost(prop, value);
             document.querySelector(`.Auto${tier}`).value = `${Item_text[tier]['name']} (＄: ${formatNum(Cost)})`;
-            Aps = CalAps();
+            Aps = calAps();
         }
         if (/^T\d+Pow$/.test(prop)) {
             const tier = prop.replace('Pow', "");
             const disTier = prop.replace('ow', "");
             document.querySelector(`.${disTier}`).textContent = `${tier}強化: ${Math.floor(value + 1)}`;
-            let Cost = CalCost(prop, value);
+            let Cost = calCost(prop, value);
             document.querySelector(`.${prop}`).value = `${tier}強化 (＄: ${formatNum(Cost)})`;
-            Aps = CalAps();
+            Aps = calAps();
         }
         return true;
     }
 });
 
 //処理
-function Cordingclk() {//クリック時処理
+function cordingClk() {//クリック時処理
     game.Money += 1 * (Math.pow(2, game.ClRank));
 }
 function gameLoop(currentTime) {//自動化
@@ -144,26 +147,28 @@ function gameLoop(currentTime) {//自動化
     //ブラウザの次の描画タイミングで、再びこのgameLoopを実行する（無限ループ）
     requestAnimationFrame(gameLoop);
 }
-function CalAps() {//Aps計算
+function calAps() {//Aps計算
 
     let num = 0;
     for (let i = 1; i <= (Itemkeys.length - 1); i++) {
         num += Item_config[`T${i}Rank`].Pow * (game[`T${i}Rank`] * Math.pow(2, game[`T${i}Pow`]));
     }
-    if(1 == zeroApsFlg){
+    if (zeroApsFlg) {
         num = 0;
     }
     document.querySelector('.aps').textContent = formatNum(num);
     return num;
 }
-function CalCost(Key, Rank) {//強化コスト計算
+function calCost(Key, Rank) {//強化コスト計算
     const item = Item_config[Key];
     if (!item) { return false };
     let Cost = item.BaceCost * Math.pow(item.multipul, Rank);
     Cost *= Math.pow(1.2, Math.floor(Rank / 5));
-    console.log('コスト: ', Key, ' = ', Cost);
+    if (isDev) {
+        console.log('コスト: ', Key, ' = ', Cost);
+    }
     return Cost;
-}//CalCost('T1Rank',game.T1Rank);
+}//calCost('T1Rank',game.T1Rank);
 function formatNum(num) {//数値の整形
     // 1万未満の場合はそのまま返す
     if (num < 10000) {
@@ -195,15 +200,20 @@ function unlockTierDOM(tier) {//非表示の解除
     if (powBtn) powBtn.classList.remove('hidden');
     if (autoSta) autoSta.classList.remove('hidden');
     if (powSta) powSta.classList.remove('hidden');
+    if (isDev) {
+        console.log(`Tier${tier}の情報を表示しました`)
+    }
 }
 
 //強化系
-function BuyUpgrade(rank) {//自動化購入
-    let Cost = CalCost(rank, game[rank]);
+function buyUpgrade(rank) {//自動化購入
+    let Cost = calCost(rank, game[rank]);
     if (game.Money >= Cost) {
         game.Money -= Cost;
         game[rank]++;
-        console.log(`${rank}購入/${rank}.`);
+        if (isDev) {
+            console.log(`${rank}購入/${rank}.`);
+        }
         return true;
     }
 }
@@ -217,11 +227,15 @@ function saveGame() {//保存処理
         localStorage.setItem(`T${i}Pow`, game[`T${i}Pow`]);
     }
     localStorage.setItem('nextTier', nextTier);
-    console.log(`進行を保存しました`);
+    if (isDev) {
+        console.log(`進行を保存しました`);
+    }
     return true;
 }
 setInterval(() => {//オートセーブ
-    console.log("オートセーブ実行...");
+    if (isDev) {
+        console.log("オートセーブ実行...");
+    }
     saveGame();
 }, 300000);
 
@@ -229,66 +243,82 @@ setInterval(() => {//オートセーブ
 function debugFlg() {
     let Flg = document.querySelector('#debugFlg');
     if (Flg.checked) {
+        isDev = 1;
         localStorage.setItem(`debugFlg`, 1);
+        console.log(`デバッグメニューを有効にしました`);
         saveGame();
-        console.log(`デバッグメニューを常に有効にします`);
     } else {
+        isDev = 0;
         localStorage.setItem(`debugFlg`, 0);
+        console.log(`デバッグメニューを無効にしました`);
         saveGame();
-        console.log(`次回起動時デバッグメニューを非表示にします`);
     }
 }
-function Mreset() {//リセット処理
-    game.Money = 0;
-    console.log(`所持金を初期化しました/Money has been reset.`);
-}
-function Creset() {//クリックリセット
-    game.ClRank = 0;
-    console.log(`クリックを初期化しました/TierUpgrade has been reset.`);
-}
-function Areset() {//完全リセット処理
-    if (confirm("本当にリセットしますか？\n*セーブデータも削除され元に戻すことができません*")) {
-        localStorage.clear();
+function mReset() {//リセット処理
+    if (isDev) {
         game.Money = 0;
+        console.log(`所持金を初期化しました/Money has been reset.`);
+    }
+}
+function cReset() {//クリックリセット
+    if (isDev) {
         game.ClRank = 0;
-        game.ClPow = 0;
+        console.log(`クリックを初期化しました/TierUpgrade has been reset.`);
+    }
+}
+function aReset() {//完全リセット処理
+    if (isDev) {
+        if (confirm("本当にリセットしますか？\n*セーブデータも削除され元に戻すことができません*")) {
+            localStorage.clear();
+            game.Money = 0;
+            game.ClRank = 0;
+            game.ClPow = 0;
+            for (let i = 1; i <= (Itemkeys.length - 1); i++) {
+                game[`T${i}Rank`] = 0;
+                game[`T${i}Pow`] = 0;
+            }
+            Aps = calAps();
+            nextTier = 1;
+            saveGame();
+            console.log(`進行を初期化しました/GameData has been reset.`);
+            location.reload();
+        }
+    }
+}
+function tReset() {//施設数リセット
+    if (isDev) {
         for (let i = 1; i <= (Itemkeys.length - 1); i++) {
             game[`T${i}Rank`] = 0;
+        }
+        console.log(`施設数を初期化しました/TierUpgrade has been reset.`);
+    }
+}
+function pReset() {//施設強化リセット
+    if (isDev) {
+        for (let i = 1; i <= (Itemkeys.length - 1); i++) {
             game[`T${i}Pow`] = 0;
         }
-        Aps = CalAps();
-        nextTier = 1;
-        saveGame();
-        console.log(`進行を初期化しました/GameData has been reset.`);
-        location.reload();
+        console.log(`施設強化を初期化しました/TierPow has been reset.`);
     }
-}
-function Treset() {//施設数リセット
-    for (let i = 1; i <= (Itemkeys.length - 1); i++) {
-        game[`T${i}Rank`] = 0;
-    }
-    console.log(`施設数を初期化しました/TierUpgrade has been reset.`);
-}
-function Preset() {//施設強化リセット
-    for (let i = 1; i <= (Itemkeys.length - 1); i++) {
-        game[`T${i}Pow`] = 0;
-    }
-    console.log(`施設強化を初期化しました/TierPow has been reset.`);
 }
 function addMoney(i) {//所持金増額
-    game.Money += 1 * Math.pow(10, i);
-    console.log(`所持金を増やしました/.`);
+    if (isDev) {
+        game.Money += 1 * Math.pow(10, i);
+        console.log(`所持金を$${1 * Math.pow(10, i)}増やしました/.`);
+    }
 }
 let zeroApsFlg = 0;
-function ZeroAps(){//Apsを0にする
+function zeroAps() {//Apsを0にする
+    if (isDev) {
         if (document.querySelector('.debugAps1').checked) {
-        zeroApsFlg = 1;
-        Aps = CalAps();
-        console.log(`Apsを0にします`);
-    } else {
-        zeroApsFlg = 0;
-        Aps = CalAps();
-        console.log(`通常のApsにします`);
+            zeroApsFlg = 1;
+            Aps = calAps();
+            console.log(`Apsを0にします`);
+        } else {
+            zeroApsFlg = 0;
+            Aps = calAps();
+            console.log(`通常のApsにします`);
+        }
     }
 }
 
@@ -337,40 +367,40 @@ for (let i = 1; i <= (Itemkeys.length - 1); i++) {
     game[`T${i}Pow`] = game[`T${i}Pow`];
     document.querySelector(`.AutoT${i}`).title = Item_text[`T${i}`]['text'];
 }
-Aps = CalAps();
+Aps = calAps();
 //起動時の非表示解除
 for (let i = 1; i < nextTier; i++) {
     unlockTierDOM(i);
 }
-if (1 == isDev) {
+if (isDev) {
     document.querySelector('.debug').removeAttribute('hidden');
     const debFlgBox = document.querySelector('#debugFlg');
     debFlgBox.checked = true;
 }
 //イベント定義
 requestAnimationFrame(gameLoop);
-document.querySelector('.cording').addEventListener(`click`, Cordingclk);
+document.querySelector('.cording').addEventListener(`click`, cordingClk);
 //アップグレード
-document.querySelector('.UpgradeClk').addEventListener(`click`, () => BuyUpgrade('ClRank'));
+document.querySelector('.UpgradeClk').addEventListener(`click`, () => buyUpgrade('ClRank'));
 //自動化強化
 for (let i = 1; i <= (Itemkeys.length - 1); i++) {
-    document.querySelector(`.AutoT${i}`).addEventListener(`click`, () => BuyUpgrade(`T${i}Rank`));//自動化個数
-    document.querySelector(`.T${i}Pow`).addEventListener(`click`, () => BuyUpgrade(`T${i}Pow`));//自動化強化
+    document.querySelector(`.AutoT${i}`).addEventListener(`click`, () => buyUpgrade(`T${i}Rank`));//自動化個数
+    document.querySelector(`.T${i}Pow`).addEventListener(`click`, () => buyUpgrade(`T${i}Pow`));//自動化強化
 }
 //その他
 document.querySelector('.save').addEventListener(`click`, saveGame);
 document.querySelector('#debugFlg').addEventListener(`change`, debugFlg);
-document.querySelector('.reset1').addEventListener(`click`, Mreset);
-document.querySelector('.reset2').addEventListener(`click`, Creset);
-document.querySelector('.reset3').addEventListener(`click`, Treset);
-document.querySelector('.reset4').addEventListener(`click`, Preset);
-document.querySelector('.resetAll').addEventListener(`click`, Areset);
+document.querySelector('.reset1').addEventListener(`click`, mReset);
+document.querySelector('.reset2').addEventListener(`click`, cReset);
+document.querySelector('.reset3').addEventListener(`click`, tReset);
+document.querySelector('.reset4').addEventListener(`click`, pReset);
+document.querySelector('.resetAll').addEventListener(`click`, aReset);
 document.querySelector('.debugM1').addEventListener(`click`, () => addMoney(4));
 document.querySelector('.debugM2').addEventListener(`click`, () => addMoney(8));
 document.querySelector('.debugM3').addEventListener(`click`, () => addMoney(12));
 document.querySelector('.debugM4').addEventListener(`click`, () => addMoney(16));
 document.querySelector('.debugM5').addEventListener(`click`, () => addMoney(20));
-document.querySelector('.debugAps1').addEventListener(`change`, ZeroAps);
-if(0 == isDev) {
+document.querySelector('.debugAps1').addEventListener(`change`, zeroAps);
+if (!(isDev)) {
     alert('コードクリッカーへようこそ！\n\n「コードを書く」をクリックしてお金を稼ぎ\n稼いだお金で更にコーディングを加速させよう！');
 }
